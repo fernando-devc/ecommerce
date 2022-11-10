7f00d9e0e0cce1e5dc6d4c4ed1e87b61c472031d
GamerNew <gamernew@pop-os.localdomain>
2022-11-10 11:24:54 -0400

    terceiro

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -29,7 +29,7 @@ export default defineComponent({
 
   setup() {
     return {
-      cart: ref(true),
+      cart: ref(false),
       toggleRightDrawer() {
         cart.value = !cart.value;
       },
